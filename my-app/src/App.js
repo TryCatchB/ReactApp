ed591116dacc7db51e7e1c70c1d18e51799cfdff
TryCatchB <pozdrakovzena@gmail.com>
2024-09-15 16:02:04 +0300

    React-App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import "./Styles/App.css";
-import "./Styles/PostComment.css";
-import "./Styles/Pagination.css";
-import "./Styles/PostInfo.css";
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./Components/UI/NavBar/NavBar";
 import { Auth, Query } from "./Hooks/useContext";
 import AppRouter from "./Components/AppRouter";
+import "./Styles/App.css";
+import "./Styles/PostComment.css";
+import "./Styles/Pagination.css";
+import "./Styles/PostInfo.css";
 
 function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
